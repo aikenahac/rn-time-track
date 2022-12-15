ae2258caf245df06e8abe4dc87d5e2ba6789e7f8
Aiken Tine Ahac <ahac.aiken@gmail.com>
2022-12-15 23:24:25 +0100

    Initial commit with base feature

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
+import { useRef, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AppState } from 'react-native';
+import { timer } from './utils';
 
 export default function App() {
+  const appState = useRef(AppState.currentState);
+
+  let tmr = timer();
+
+  useEffect(() => {
+    const sub = AppState.addEventListener('change', (next) => {
+      appState.current = next;
+      console.log('AppState', appState.current);
+
+      if (appState.current === 'active') tmr = timer();
+      else if (appState.current === 'background') {
+        console.log(tmr.seconds);
+        // Make an API call here to send the timer data somewhere
+      }
+    });
+
+    return () => {
+      sub.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Close the app to send the timer data</Text>
       <StatusBar style="auto" />
     </View>
   );
